074cf2bbdb69de9e4d52addf381a5f3997c9c466
AlexAguado24 <aaguado131@gmail.com>
2022-09-16 09:55:50 +0200

    .

---
+++ T1/codigos2223/EjercicioCartas/src/model/Carta.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Carta {
+
+    private int valor;
+    private String palo;
+
+    public Carta() {}
+
+    public Carta(int valor, String palo) {
+        this.valor = valor;
+        this.palo = palo;
+    }
+
+
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public String getPalo() {
+        return palo;
+    }
+
+    public void setPalo(String palo) {
+        this.palo = palo;
+    }
+
+    @Override
+    public String toString() {
+        return "Carta{" +
+                "valor=" + valor +
+                ", palo='" + palo + '\'' +
+                '}';
+    }
+}
