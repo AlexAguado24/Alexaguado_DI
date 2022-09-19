61adb5ee7d5763aad9a1f6f84d6be0d78dd55712
AlexAguado24 <aaguado131@gmail.com>
2022-09-19 21:44:47 +0200

    .

---
+++ T1/codigos2223/Repaso/src/Ej5/Main.java
@@ -0,0 +1,43 @@
+package Ej5;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int numAleatorio = (int)(Math.random()*101);
+
+        int intentos = 0, respuesta;
+        int mediaInt = 0, minIntentos = 50;
+        String respuesta2 = "";
+        System.out.println("Indica el numero del sorteo");
+        do {
+            if (respuesta2.equalsIgnoreCase("s")){
+                numAleatorio = (int)(Math.random()*101);
+            }
+            respuesta = scanner.nextInt();
+
+            if (respuesta > numAleatorio){
+                System.out.println("El numero mayor, intetalo otra vez");
+            }
+            if (respuesta < numAleatorio){
+                System.out.println("El numero es menor, intentalo otra vez");
+            }
+            if (respuesta == numAleatorio){
+                System.out.println("Enhorabuena, el numero era "+ numAleatorio);
+                System.out.println("EL numero de intentos ha sido"+ intentos);
+                System.out.println("Quieres volver a intentarlo?");
+                respuesta2 = scanner.next();
+                intentos = 0;
+            }
+            intentos++;
+
+        } while (!respuesta2.equalsIgnoreCase("n"));
+
+
+    }
+
+    public int numeroAleatorio
+}
