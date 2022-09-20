412ba10805ea883bf9211c043440f57af028be43
AlexAguado24 <aaguado131@gmail.com>
2022-09-20 21:22:20 +0200

    .

---
+++ T1/codigos2223/EjercicioGasolinera/src/model/Surtidor.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Surtidor {
+
+    private final int capacidadTotal = 1000;
+    private int capacidadActual;
+    private String tipo;
+    private boolean funciona;
+
+    
+
+}
