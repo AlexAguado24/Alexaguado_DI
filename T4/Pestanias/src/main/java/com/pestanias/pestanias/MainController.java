2a5775150be4b975b7f06533404882ae0f60549b
AlexAguado24 <aaguado131@gmail.com>
2022-10-11 12:46:19 +0200

    .

--- T4/Pestanias/src/main/java/com/pestanias/pestanias/MainController.java
+++ T4/Pestanias/src/main/java/com/pestanias/pestanias/MainController.java
@@ -7,6 +7,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TabPane;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
@@ -20,20 +22,44 @@ public class MainController implements Initializable {
     private DropShadow sombraExterior;
     @FXML
     private Button botonNormal, botonNormalDos;
+
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         // se ejecuta cuando se asocia la parte grafica y la logica
-
-        sombraExterior = new DropShadow();
         /*botonNormal.setEffect(sombraExterior);
         botonNormalDos.setEffect(sombraExterior);*/
+        instancias();
+        configurarBotones();
+        acciones();
+    }
+
+    private void configurarBotones(){
+        //imageView --> image
+
+        botonNormal.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("power_on.png"))));
+        botonNormal.setBackground(null);
+
+
+        botonNormalDos.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("power_off.png"))));
+        botonNormalDos.setBackground(null);
+
+    }
+
+    private void instancias(){
+        sombraExterior = new DropShadow();
+    }
+
+    private void acciones(){
         botonNormal.setOnAction(new ManejoPulsaciones());
         botonNormalDos.setOnAction(new ManejoPulsaciones());
         botonNormal.addEventHandler(MouseEvent.MOUSE_ENTERED, new ManejoRaton());
         botonNormalDos.addEventHandler(MouseEvent.MOUSE_ENTERED, new ManejoRaton());
         botonNormal.addEventHandler(MouseEvent.MOUSE_EXITED, new ManejoRaton());
         botonNormalDos.addEventHandler(MouseEvent.MOUSE_EXITED, new ManejoRaton());
-
+        botonNormal.addEventHandler(MouseEvent.MOUSE_PRESSED, new ManejoRaton());
+        botonNormal.addEventHandler(MouseEvent.MOUSE_CLICKED, new ManejoRaton());
     }
 
     class ManejoRaton implements EventHandler<MouseEvent>{
@@ -46,13 +72,23 @@ public class MainController implements Initializable {
                 } else if(mouseEvent.getSource() == botonNormalDos) {
                     botonNormalDos.setEffect(sombraExterior);
                 }
-            } else if (mouseEvent.getEventType() == MouseEvent.MOUSE_EXITED){
+            }
+            else if (mouseEvent.getEventType() == MouseEvent.MOUSE_EXITED){
                 System.out.println("Evento raton saliente");
                 if (mouseEvent.getSource() == botonNormal) {
                     botonNormal.setEffect(null);
                 } else if(mouseEvent.getSource() == botonNormalDos) {
                     botonNormalDos.setEffect(null);
                 }
+            }
+            else if (mouseEvent.getEventType() == MouseEvent.MOUSE_PRESSED) {
+                System.out.println("Boton Pressed");
+                botonNormal.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("power_off.png"))));
+            }
+            else if (mouseEvent.getEventType() == MouseEvent.MOUSE_CLICKED) {
+                System.out.println("Boton Clicked");
+                botonNormal.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("power_on.png"))));
+
             }
         }
     }
