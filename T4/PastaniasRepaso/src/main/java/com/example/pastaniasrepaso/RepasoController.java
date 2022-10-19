201100912cf3ab2b706ede98374d7c8c6d5fb3b6
AlexAguado24 <aaguado131@gmail.com>
2022-10-19 20:18:33 +0200

    .

---
+++ T4/PastaniasRepaso/src/main/java/com/example/pastaniasrepaso/RepasoController.java
@@ -0,0 +1,23 @@
+package com.example.pastaniasrepaso;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TabPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RepasoController implements Initializable {
+
+    @FXML
+    private TabPane panelPestanias;
+
+    public RepasoController() {
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
