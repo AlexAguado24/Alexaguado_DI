28aebda6d10ec47e1c99f33f54b2c81a1eae4399
AlexAguado24 <aaguado131@gmail.com>
2022-10-24 11:15:15 +0200

    .

--- T4/Pestanias/src/main/java/com/pestanias/pestanias/MainController.java
+++ T4/Pestanias/src/main/java/com/pestanias/pestanias/MainController.java
@@ -1,8 +1,11 @@
 package com.pestanias.pestanias;
 
 import com.pestanias.pestanias.model.TipoDePago;
+import com.pestanias.pestanias.model.Usuario;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -32,9 +35,22 @@ public class MainController implements Initializable {
     @FXML
     private TextField textField1, textField2;
     @FXML
-    private Button botonSuma,botonResta, botonMultiplicacion, botonDivision, botonIgual;
+    private Button botonSuma,botonResta, botonMultiplicacion, botonDivision, botonIgual, botonComprobar;
     @FXML
     private GridPane gridBotones;
+
+    @FXML
+    private ChoiceBox<String> choice;
+    @FXML
+    private ComboBox<String> combo;
+    @FXML
+    private ComboBox<Usuario> comboUsuario;
+    @FXML
+    private Spinner<String> spinner;
+
+    private ObservableList<String> listaChoice, listaCombo, listaSpinner;
+    private ObservableList<Usuario> listaUsuarios;
+
     private ToggleGroup grupoRadios;
     private DropShadow sombraExterior;
     private int tipoOperacion = -1;
@@ -54,6 +70,9 @@ public class MainController implements Initializable {
         radio1.setUserData(new TipoDePago("Tarjeta","Pago con tarjeta bancaria",0));
         radio2.setUserData(new TipoDePago("Transferencia","Pago con tarjeta bancaria",10));
         radio3.setUserData(new TipoDePago("PayPal","Pago con Paypal",20));
+
+        combo.setItems(listaCombo);
+        choice.setItems(listaChoice);
     }
 
     private void configurarBotones(){
@@ -74,6 +93,20 @@ public class MainController implements Initializable {
         sombraExterior = new DropShadow();
         grupoRadios = new ToggleGroup();
         grupoRadios.getToggles().addAll(radio1,radio2,radio3);
+
+        listaChoice = FXCollections.observableArrayList();
+        listaChoice.addAll("OpcionCH 1","OpcionCH 2","OpcionCH 3","OpcionCH 4","OpcionCH 5");
+        listaCombo = FXCollections.observableArrayList();
+        listaCombo.addAll("OpcionCB 1","OpcionCB 2","OpcionCB 3","OpcionCB 4","OpcionCB 5");
+        listaSpinner = FXCollections.observableArrayList();
+        listaSpinner.addAll("OpcionSP 1","OpcionSP 2", "OpcionSP 3", "OpcionSP 4", "OpcionSP 5");
+
+        listaUsuarios = FXCollections.observableArrayList();
+        listaUsuarios.addAll(new Usuario(1,"Paco","Martinez", "pacoinalaysequedaduro@gmail.com"));
+        listaUsuarios.addAll(new Usuario(1,"Marcos","Salas", "selapegaenlapera@gmail.com"));
+        listaUsuarios.addAll(new Usuario(1,"Andres","Zama", "zamarreñoboy@gmail.com"));
+        listaUsuarios.addAll(new Usuario(1,"Marta","Pacheco", "lapachequita@gmail.com"));
+        listaUsuarios.addAll(new Usuario(1,"Patri","Fraile", "frailecillo@gmail.com"));
     }
 
     private void acciones(){
@@ -116,7 +149,28 @@ public class MainController implements Initializable {
                 System.out.println(((TipoDePago)radioButton.getUserData()).getComision());
             }
         });
-
+        botonComprobar.setOnAction(new ManejoPulsaciones());
+        combo.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String newValue) {
+                System.out.println("El valor cambiado del combo es "+ newValue);
+            }
+        });
+        choice.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String newValue) {
+                System.out.println("La seleccion del choice es "+newValue);
+            }
+        });
+        comboUsuario.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Usuario>() {
+            @Override
+            public void changed(ObservableValue<? extends Usuario> observableValue, Usuario usuario, Usuario newValue) {
+                System.out.println("ID "+ newValue.getId());
+                System.out.println("Nombre "+ newValue.getNombre());
+                System.out.println("Apellido "+ newValue.getApellido());
+                System.out.println("correo "+ newValue.getCorreo());
+            }
+        });
     }
 
     class ManejoRaton implements EventHandler<MouseEvent>{
@@ -169,7 +223,27 @@ public class MainController implements Initializable {
             } else if (actionEvent.getSource() == botonDivision) {
             } else if (actionEvent.getSource() == botonIgual) {
 
+            } else if (actionEvent.getSource() == botonComprobar) {
+                // seleccion de una lista
+
+                String seleccionCombo = combo.getSelectionModel().getSelectedItem();
+                String seleccionChoice = choice.getSelectionModel().getSelectedItem();
+                String usuarioNombre = comboUsuario.getSelectionModel().selectedItemProperty().getName();
+
+                System.out.println(combo.getSelectionModel().getSelectedItem());
+                System.out.println(choice.getSelectionModel().getSelectedItem());
+                System.out.println(comboUsuario.getSelectionModel().getSelectedItem());
+
+
+                if (combo.getSelectionModel().getSelectedIndex() >-1 && choice.getSelectionModel().getSelectedIndex()>-1){
+                    System.out.printf("Seleccion de combo %s%n",seleccionCombo);
+                    System.out.printf("Seleccion de choice %s%n",seleccionChoice);
+                    System.out.printf("Seleccion de choice %s%n",usuarioNombre);
+                } else {
+                    System.out.println("Uno de los dos elementos no tiene seleccion");
+                }
             }
+
         }
     }
 }
