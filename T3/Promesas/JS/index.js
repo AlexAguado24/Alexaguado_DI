31cf5bbcedfa02374992081651234cb5004c30f8
AlexAguado24 <aaguado131@gmail.com>
2023-01-16 11:30:14 +0100

    .

--- T3/Promesas/JS/index.js
+++ T3/Promesas/JS/index.js
@@ -1,15 +1,22 @@
 let urlString = "https://dummyjson.com/products/categories";
-
-fetch(urlString)
-.then((res) => res.json()
-).then((res1) => {
-    res1.forEach(element => {
-        console.log(`${element}`);
+ 
+// jdbc
+// connection.createStatement().executeQuery()
+let selecciones = document.querySelector('select')
+fetch(urlString).then((res) => res.json())
+  .then((res1) => {
+    res1.forEach(item => {
+    console.log( `${item}`  )
+    let option = document.createElement('option')
+    option.value=item
+    option.innerText=item
+    selecciones.append(option)
     });
-})
-.catch((err) => {
-    console.log("fetch resulta sin exito");
-})
-.finally(() => {
+  })
+  .catch((err) => {
+    console.log("fetch resuelta sin exito");
+  })
+  .finally(() => {
     console.log("Fin de la evaluacion de la promesa");
-});
+  });
+
