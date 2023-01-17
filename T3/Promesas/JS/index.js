efffafc92c3a0a384edbd3de072990e4b234bb19
AlexAguado24 <aaguado131@gmail.com>
2023-01-17 10:20:49 +0100

    .

--- T3/Promesas/JS/index.js
+++ T3/Promesas/JS/index.js
@@ -1,8 +1,9 @@
-let urlString = "https://dummyjson.com/products/categories";
+let urlString = "https://dummyjson.com/products";
+let urlStringCategorias = "https://dummyjson.com/products/categories";
  
 // jdbc
 // connection.createStatement().executeQuery()
-let selecciones = document.querySelector('select')
+/* let selecciones = document.querySelector('select')
 fetch(urlString).then((res) => res.json())
   .then((res1) => {
     res1.forEach(item => {
@@ -18,5 +19,33 @@ fetch(urlString).then((res) => res.json())
   })
   .finally(() => {
     console.log("Fin de la evaluacion de la promesa");
-  });
+  }); */
 
+
+let selectCategorias = document.querySelector("select")
+
+function cargarProductos() {
+  fetch(urlString)
+  .then((res)=>{
+
+  }).then((res1)=>{
+
+  }).catch((err)=>{
+    
+  })
+}
+
+
+function cargarCategorias() {
+  fetch(urlStringCategorias)
+.then((res) => {
+  return res.json();
+}).then((res1) => {
+  res1.forEach(element => {
+    selectCategorias.innerHTML += `<option value="${element}">${element}</option>`;
+  });
+})
+.catch((err) => {
+  console.log(`Error en la empresa ${err}`);
+})
+}
