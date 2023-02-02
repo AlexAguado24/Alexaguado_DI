35dccd5d76c4c8f0984f3fa8e7c617ba1f866cab
AlexAguado24 <aaguado131@gmail.com>
2023-02-02 13:45:27 +0100

    Angular

--- T3/Proyecto_TS/index.ts
+++ T3/Proyecto_TS/index.ts
@@ -1,1 +1,25 @@
-import {ejemplo} from "./js/funciones"
+//function funcionNormal(param1:Number,param2: Number ): Number {}
+
+
+function optativo(param1:String,param2: String, p3?: Number ) {
+    console.log(param1);
+    console.log(param2);
+    console.log(p3);
+}
+
+
+function defecto(p1:String, p2:String, p3:Number = 2 ) {
+    console.log(p1);
+    console.log(p2);
+    console.log(p3);
+}
+
+class alumno {
+
+    constructor(private nombre:String,
+        private apellido:String,
+        private correo:String)
+         {
+    }
+}
+
