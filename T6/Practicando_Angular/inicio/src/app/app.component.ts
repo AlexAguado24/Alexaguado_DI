814904a4ce367a4018d233cc1bf0cd512a44075c
AlexAguado24 <aaguado131@gmail.com>
2023-02-07 20:16:34 +0100

    .

---
+++ T6/Practicando_Angular/inicio/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'miapp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  asignatura = 'dont fuck with me';
+}
