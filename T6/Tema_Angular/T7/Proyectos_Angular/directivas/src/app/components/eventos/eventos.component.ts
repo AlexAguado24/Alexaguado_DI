132981ad88d7c33ae28b97b1578cb63fd99a50a3
AlexAguado24 <aaguado131@gmail.com>
2023-02-08 12:11:23 +0100

    .

--- T6/Tema_Angular/T7/Proyectos_Angular/directivas/src/app/components/eventos/eventos.component.ts
+++ T6/Tema_Angular/T7/Proyectos_Angular/directivas/src/app/components/eventos/eventos.component.ts
@@ -7,14 +7,21 @@ import { Component } from '@angular/core';
 })
 export class EventosComponent {
   alumno = {
-    nombre: `Borja`,
-    apellido: `Martin`,
-    edad: 40
+    nombre: ``,
+    apellido: ``,
+    estudios:``,
+    ciclo:``,
+    experiencia: Boolean
   }
   urlImagen = 'src=https://assets.stickpng.com/images/5847ea22cef1014c0b5e4833.png'
 
-  pulsacionBotonInicial (nombre: string, edad:Number) {
+  pulsacionBotonInicial (nombre: string, apellido: string, estudios: string, ciclo: string ) {
     /* console.log(`nombre pasado: ${nombre}, edad pasada: ${edad}`); */
-    this.alumno.nombre = nombre;
+    /* this.alumno.nombre = nombre; */
+    this.alumno.nombre = nombre
+    this.alumno.apellido = apellido
+    this.alumno.estudios = estudios
+    this.alumno.ciclo = ciclo
+
   }
 }
