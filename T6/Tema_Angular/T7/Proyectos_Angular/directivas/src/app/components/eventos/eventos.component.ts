a304c2c85b2ddbc15b9f09a120983ba288f729f2
AlexAguado24 <aaguado131@gmail.com>
2023-02-13 14:27:52 +0100

    .

--- T6/Tema_Angular/T7/Proyectos_Angular/directivas/src/app/components/eventos/eventos.component.ts
+++ T6/Tema_Angular/T7/Proyectos_Angular/directivas/src/app/components/eventos/eventos.component.ts
@@ -11,16 +11,41 @@ export class EventosComponent {
     apellido: ``,
     estudios:``,
     ciclo:``,
-    experiencia: Boolean
-  }
+    experiencia: false
+  };
+  estudio = ''
+  estado = false
   urlImagen = 'src=https://assets.stickpng.com/images/5847ea22cef1014c0b5e4833.png'
 
-  pulsacionBotonInicial (nombre: string, apellido: string, estudios: string, ciclo:any) {
+  pulsacionBotonInicial (nombre: string, apellido: string, estudios: string, ciclo:string) {
     /* console.log(`nombre pasado: ${nombre}, edad pasada: ${edad}`); */
     /* this.alumno.nombre = nombre; */
     this.alumno.nombre = nombre
     this.alumno.apellido = apellido
     this.alumno.estudios = estudios
-    this.alumno.ciclo = ciclo
+    this.alumno.ciclo = this.estudio
+
+  }
+
+  capturaValores(nombre:string,apellido:string,estudios:string) {
+    if (nombre&&apellido&&estudios) {
+      this.alumno.nombre = nombre
+      this.alumno.apellido = apellido
+      this.alumno.estudios = estudios
+      this.alumno.ciclo = this.estudio
+      this.alumno.experiencia = this.estado
+    } else {
+      alert("Algun dato esta mal")
+    }
+
+  }
+  capturaCambio(ciclo:string) {
+    this.estudio = ciclo;
+  }
+  cambioEstado(experiencia:boolean) {
+    this.estado = experiencia
+  }
+  capturaEvento(evento:any) {
+    evento.target.value
   }
 }
