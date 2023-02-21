e2d61f81745deb95c44054553297b778954c5e27
AlexAguado24 <aaguado131@gmail.com>
2023-02-21 09:26:48 +0100

    .

---
+++ T6/Tema_Angular/T7/Proyectos_Angular/directivas/src/app/components/directivafor/directivafor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'directivas-directivafor',
+  templateUrl: './directivafor.component.html',
+  styleUrls: ['./directivafor.component.css']
+})
+export class DirectivaforComponent {
+
+}
